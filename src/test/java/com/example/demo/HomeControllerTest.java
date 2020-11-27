443cfeadd58408b56bf4a3f53fe63ce96dc44b81
EmilNordahn <60731699+EmilNordahn@users.noreply.github.com>
2020-11-27 15:41:12 +0100

    Tilføjet test der altid fejler

--- src/test/java/com/example/demo/HomeControllerTest.java
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -3,13 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-class TechPipelineGradleTestApplicationTests {
+class HomeControllerTest {
 
     @Test
-    void contextLoads() {
+    public void test(){
+        assertFalse(true);
     }
-
-
 
 }
